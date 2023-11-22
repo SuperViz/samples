86161a26932dea0d372eb463e7270b253f34226b
Ian Silva <raspincel@gmail.com>
2023-11-22 17:03:30 -0300

    feat: create sample of WIO using React and Js

---
+++ src/components/WhoIsOnlineContainer.jsx
@@ -0,0 +1,35 @@
+import SuperVizRoom from '@superviz/sdk'
+import { WhoIsOnline } from '@superviz/sdk/lib/components'
+
+const groupId = "sv-sample-room-react-mouse-pointers";
+const groupName = "Sample Room for Mouse Pointers (React + TS)";
+const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY
+
+const initWhoIsOnline = async (roomId, userId, name, containerId)=> {
+  const room = await SuperVizRoom(DEVELOPER_KEY, {
+    roomId: roomId,
+    group: {
+      id: groupId,
+      name: groupName,
+    },
+    participant: {
+      id: userId,
+      name: name,
+    },
+  });
+
+  const whoIsOnline = new WhoIsOnline(containerId)
+  room.addComponent(whoIsOnline);
+}
+
+export default function WhoIsOnlineContainer({ name, roomId }) {
+    const containerId = name + '-canvas'
+    const userId = name.toLowerCase()
+
+    return (
+      <section>
+          <button onClick={()=>initWhoIsOnline(roomId, userId, name, containerId)}>Enter Who Is Online as "{name}"</button>
+          <div id={containerId}/>
+      </section>
+    )
+}
