833ec26d4abd9ede4de0164e1295a955b9d4ed2b
Vitor Norton <vtnorton@outlook.com>
2023-11-23 15:21:54 -0300

    Fixes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,6 @@
 import { DEVELOPER_KEY, MATTERPORT_KEY } from "../env.js";
 
-const roomId = generateUUID();
+const roomId = "AAd689fe-03b0-442f-ba5e-fb0bbd39d983";
 const groupId = "sv-sample-room-cdn-js-presence3d-matterport";
 const groupName = "Sample Room for Presence3D for Matterport (CDN/JS)";
 const modelId = "LmRnZAsWoxy";
