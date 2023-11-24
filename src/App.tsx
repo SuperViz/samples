c84a750300acb7011ee32b962d433706e1841948
Vitor Norton <vtnorton@outlook.com>
2023-11-24 15:37:30 -0300

    Improved code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import './index.css'
-import CommentsInstance from './components/CommentsInstance'
-import { useRef, useState } from 'react'
-import { LauncherFacade } from '@superviz/sdk'
+import "./index.css";
+import CommentsInstance from "./components/CommentsInstance";
+import { useRef, useState } from "react";
+import { LauncherFacade } from "@superviz/sdk";
 
 function App() {
-  const [showZeus, setShowZeus] = useState(true)
-  const room1 = useRef<LauncherFacade>()
-  const room2 = useRef<LauncherFacade>()
+  const [showZeus, setShowZeus] = useState(true);
+  const room1 = useRef<LauncherFacade>();
+  const room2 = useRef<LauncherFacade>();
 
   const toggle = () => {
-    setShowZeus(!showZeus)
-  }
+    setShowZeus(!showZeus);
+  };
 
   const props = {
-    name: showZeus ? 'Zeus' : 'Hera',
-    position: showZeus ? 'right' : 'left',
+    name: showZeus ? "Zeus" : "Hera",
+    position: showZeus ? "right" : "left",
     room: showZeus ? room1 : room2,
-  } as { name: string; position: 'left' | 'right', room: React.MutableRefObject<LauncherFacade | undefined> }
+  } as { name: string; position: "left" | "right"; room: React.MutableRefObject<LauncherFacade | undefined> };
 
-  return <CommentsInstance {...props} toggle={toggle} key={String(showZeus)}/>
+  return <CommentsInstance {...props} toggle={toggle} key={String(showZeus)} />;
 }
 
-export default App
+export default App;
--- src/components/CommentsInstance.tsx
+++ src/components/CommentsInstance.tsx
@@ -1,24 +1,24 @@
-import SuperVizRoom, { LauncherFacade } from '@superviz/sdk'
-import { Comments, CanvasPin } from '@superviz/sdk/lib/components'
-import { useEffect, useRef, useState } from 'react';
+import SuperVizRoom, { LauncherFacade } from "@superviz/sdk";
+import { Comments, CanvasPin } from "@superviz/sdk/lib/components";
+import { useEffect, useRef, useState } from "react";
 
-const groupId = "sv-sample-room-react-ts-mouse-pointers";
-const groupName = "Sample Room for Mouse Pointers (React/TS)";
+const roomId = "349105d6-3a67-41a9-9b74-59127fd115d9";
+const groupId = "sv-sample-room-react-ts-contextual-comments-html";
+const groupName = "Sample Room for Contextual Comments for HTML (React/TS)";
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
 
-interface Props { 
-  name: string; 
-  position: 'left' | 'right' 
-  toggle: ()=> void
+interface Props {
+  name: string;
+  position: "left" | "right";
+  toggle: () => void;
 }
 
 export default function CommentsInstance({ name, position, toggle }: Props) {
-  const [room, setRoom] = useState<LauncherFacade>()
-  const [comments, setComments] = useState<Comments>()
-  
+  const participantId = name.toLowerCase();
   const loaded = useRef<boolean>(false);
+  const [room, setRoom] = useState<LauncherFacade>();
 
-  const initComments = async (room: LauncherFacade | undefined, roomId: string, userId: string, name: string, position: 'left' | 'right') => {
+  const initSuperVizWithComments = async (room: LauncherFacade | undefined, position: "left" | "right") => {
     room = await SuperVizRoom(DEVELOPER_KEY, {
       roomId: roomId,
       group: {
@@ -26,43 +26,43 @@ export default function CommentsInstance({ name, position, toggle }: Props) {
         name: groupName,
       },
       participant: {
-        id: userId,
+        id: participantId,
         name: name,
       },
       environment: "dev" as any,
     });
-  
-    const pinAdapter = new CanvasPin(`${name.toLowerCase()}-participant`);
-    const comments = new Comments(pinAdapter, { position, buttonLocation: `top-${position}` })
+
+    const pinAdapter = new CanvasPin(`${participantId}-participant`);
+    const comments = new Comments(pinAdapter, { position, buttonLocation: `top-${position}` });
 
     room.addComponent(comments);
+
     setRoom(room as LauncherFacade);
-  }
+  };
 
+  useEffect(() => {
+    if (loaded.current) return;
+    loaded.current = true;
 
-  useEffect(()=> {
-      if (loaded.current) return;
-      loaded.current = true;
-    
-      ( async () => { 
-          await initComments(room, "sv-sample-room-react-ts-mouse-pointers", name.toLowerCase(), name, position);
-        }
-      )()
-    }, [])
+    (async () => {
+      await initSuperVizWithComments(room, position);
+    })();
+  }, []);
 
-    const destroy = () => {
-      if (room) {
-        room.destroy()
-        toggle()
-      }
+  const destroy = () => {
+    if (room) {
+      room.destroy();
+      toggle();
     }
-    return(
+  };
+
+  return (
     <>
       <button onClick={destroy}>Change participant</button>
       <section>
-          <h1>View from "{name}" participant</h1>
-          <canvas id={`${name.toLocaleLowerCase()}-participant`}  className={name.toLocaleLowerCase()}> </canvas>
+        <h1>View from "{name}" participant</h1>
+        <canvas id={`${name.toLocaleLowerCase()}-participant`} className={name.toLocaleLowerCase()}></canvas>
       </section>
     </>
-    )
+  );
 }
