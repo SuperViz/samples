ffd5c1a246d494b32cf3ed7bd06f2cd9e6ef5ed1
Vitor Norton <vtnorton@outlook.com>
2023-11-24 15:41:50 -0300

    Adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import './index.css'
-import CommentsInstance from './components/CommentsInstance'
-import { useRef, useState } from 'react'
+import "./index.css";
+import CommentsInstance from "./components/CommentsInstance";
+import { useRef, useState } from "react";
 
 function App() {
-  const [showZeus, setShowZeus] = useState(true)
-  const room1 = useRef()
-  const room2 = useRef()
+  const [showZeus, setShowZeus] = useState(true);
+  const room1 = useRef();
+  const room2 = useRef();
 
   const toggle = () => {
-    setShowZeus(!showZeus)
-  }
+    setShowZeus(!showZeus);
+  };
 
   const props = {
-    name: showZeus ? 'Zeus' : 'Hera',
-    position: showZeus ? 'right' : 'left',
+    name: showZeus ? "Zeus" : "Hera",
+    position: showZeus ? "right" : "left",
     room: showZeus ? room1 : room2,
   };
 
-  return <CommentsInstance {...props} toggle={toggle} key={String(showZeus)}/>
+  return <CommentsInstance {...props} toggle={toggle} key={String(showZeus)} />;
 }
 
-export default App
+export default App;
