fa098267d26f913f3fbc10a180bd152e9bb2557d
Vitor Norton <vtnorton@outlook.com>
2023-11-27 15:55:45 -0300

    Fixes to package json

--- src/components/MatterportInstance.jsx
+++ src/components/MatterportInstance.jsx
@@ -3,8 +3,8 @@ import { Comments } from "@superviz/sdk/lib/components";
 import { MatterportPin } from "@superviz/matterport-plugin";
 import { useRef, useState } from "react";
 
-const groupId = "sv-sample-room-react-ts-who-is-online";
-const groupName = "Sample Room for Who-is-Online (React/TS)";
+const groupId = "sv-sample-room-react-js-contextual-comments-matterport";
+const groupName = "Sample Room for Matterport with Contextual Comments (React/JS)";
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
 const MATTERPORT_KEY = import.meta.env.VITE_MATTERPORT_KEY;
 const modelId = "LmRnZAsWoxy";
@@ -12,64 +12,72 @@ const modelId = "LmRnZAsWoxy";
 export default function MatterportInstance({ name, roomId, avatar, position, toggle }) {
   const containerId = name + "-container";
   const userId = name.toLowerCase();
-  const ref = useRef(null)
-  const [room, setRoom] = useState()
-  const [loaded, setLoaded] = useState(false)
+  const ref = useRef(null);
+  const [room, setRoom] = useState();
+  const [loaded, setLoaded] = useState(false);
 
   const initMatterport = async (roomId, userId, name, avatar, pinAdapter, position) => {
-  const room = await SuperVizRoom(DEVELOPER_KEY, {
-    roomId: roomId,
-    group: {
-      id: groupId,
-      name: groupName,
-    },
-    participant: {
-      id: userId,
-      name: name,
-      avatar: {
-        imageUrl: `https://production.cdn.superviz.com/static/default-avatars/${avatar}.png`,
-        model3DUrl: `https://production.storage.superviz.com/readyplayerme/${avatar}.glb`,
-      }
-    },
-    environment: "dev",
-  });
+    const room = await SuperVizRoom(DEVELOPER_KEY, {
+      roomId: roomId,
+      group: {
+        id: groupId,
+        name: groupName,
+      },
+      participant: {
+        id: userId,
+        name: name,
+        avatar: {
+          imageUrl: `https://production.cdn.superviz.com/static/default-avatars/${avatar}.png`,
+          model3DUrl: `https://production.storage.superviz.com/readyplayerme/${avatar}.glb`,
+        },
+      },
+      environment: "dev",
+    });
 
-  const oppositeSide = position === 'left' ? 'right' : 'left';
+    const oppositeSide = position === "left" ? "right" : "left";
 
-  const comments = new Comments(pinAdapter, {
-    position,
-    buttonLocation: `top-${oppositeSide}`
-  });
+    const comments = new Comments(pinAdapter, {
+      position,
+      buttonLocation: `top-${oppositeSide}`,
+    });
 
-  room.addComponent(comments)
-  setRoom(room)
+    room.addComponent(comments);
+    setRoom(room);
   };
 
-  const enter = async ()=> {
-    const showcase = ref.current
+  const enter = async () => {
+    const showcase = ref.current;
     const showcaseWindow = showcase.contentWindow;
 
-    if(!showcaseWindow) return;
-    const mpSdk = await showcaseWindow.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY);   
-  
-    const pinAdapter = new MatterportPin(mpSdk, showcase);  
+    if (!showcaseWindow) return;
+    const mpSdk = await showcaseWindow.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY);
+
+    const pinAdapter = new MatterportPin(mpSdk, showcase);
     await initMatterport(roomId, userId, name, avatar, pinAdapter, position);
-    setLoaded(true)
-  }
+    setLoaded(true);
+  };
 
   const destroy = () => {
     if (room) {
-      room.destroy()
-      toggle()
+      room.destroy();
+      toggle();
     }
-  }
+  };
   return (
     <>
-    <button disabled={!loaded} onClick={destroy}>Change participant</button>
-    <section>
-      <h1>View from "{name}" participant</h1>
-      <iframe onLoad={enter} className={name.toLowerCase()} ref={ref} id={containerId} src={`/mp-bundle/showcase.html?&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=${modelId}`}/>
-    </section>
+      <button disabled={!loaded} onClick={destroy}>
+        Change participant
+      </button>
+      <section>
+        <h1>View from "{name}" participant</h1>
+        <iframe
+          onLoad={enter}
+          className={name.toLowerCase()}
+          ref={ref}
+          id={containerId}
+          src={`/mp-bundle/showcase.html?&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=${modelId}`}
+        />
+      </section>
     </>
   );
 }
