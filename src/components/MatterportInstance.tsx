58db8d55c6be75db990a5f0fc20ecc91011f2e1e
Vitor Norton <vtnorton@outlook.com>
2023-11-27 19:06:54 -0300

    Updates to matterport and threeJS

--- src/components/MatterportInstance.tsx
+++ src/components/MatterportInstance.tsx
@@ -1,66 +1,75 @@
 import SuperVizRoom from "@superviz/sdk";
 import { Presence3D } from "@superviz/matterport-plugin";
 import { useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const groupId = "sv-sample-room-react-ts-presence3d-matterport";
-const groupName = "Sample Room for Presence3D for Matterport (React/TS)";
-const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
-const MATTERPORT_KEY = import.meta.env.VITE_MATTERPORT_KEY;
-const modelId = "LmRnZAsWoxy";
+export default function MatterportInstance({ name }: { name: string }) {
+  const avatarNumber = name === "Zeus" ? "5" : "2";
+  const participantId = name.toLowerCase();
+  const roomId = uuidv4();
+  const modelId = "LmRnZAsWoxy";
+  const groupId = "sv-sample-room-react-ts-presence3d-matterport";
+  const groupName = "Sample Room for Presence3D for Matterport (React/TS)";
+  const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
+  const MATTERPORT_KEY = import.meta.env.VITE_MATTERPORT_KEY;
 
-const initSuperVizWithMatterport = async (roomId: string, userId: string, name: string, avatar: string, mpSdk: any) => {
-  const room = await SuperVizRoom(DEVELOPER_KEY, {
-    roomId: roomId,
-    group: {
-      id: groupId,
-      name: groupName,
-    },
-    participant: {
-      id: userId,
-      name: name,
-      avatar: {
-        imageUrl: `https://production.cdn.superviz.com/static/default-avatars/${avatar}.png`,
-        model3DUrl: `https://production.storage.superviz.com/readyplayerme/${avatar}.glb`,
-      },
-    },
-    environment: "dev" as any,
-  });
-
-  const matterportPresence = new Presence3D(mpSdk, {
-    isAvatarsEnabled: true,
-    isLaserEnabled: true,
-    isNameEnabled: true,
-    avatarConfig: {
-      height: 0,
-      scale: 1,
-      laserOrigin: { x: 0, y: 0, z: 0 },
-    },
-  });
-
-  room.addComponent(matterportPresence as any);
-};
-
-export default function MatterportInstance({ name, roomId, avatar }: { name: string; roomId: string; avatar: string }) {
-  const containerId = name + "-container";
-  const userId = name.toLowerCase();
-  const ref = useRef<any>(null)
+  const ref = useRef<any>(null);
   const [disableButton, setDisableButton] = useState(false);
 
-  const enter = async ()=> {
+  const initSuperVizWithMatterport = async (mpSdk: any) => {
+    const room = await SuperVizRoom(DEVELOPER_KEY, {
+      roomId: roomId,
+      group: {
+        id: groupId,
+        name: groupName,
+      },
+      participant: {
+        id: participantId,
+        name: name,
+        avatar: {
+          imageUrl: `https://production.cdn.superviz.com/static/default-avatars/${avatarNumber}.png`,
+          model3DUrl: `https://production.storage.superviz.com/readyplayerme/${avatarNumber}.glb`,
+        },
+      },
+      environment: "dev" as any,
+    });
+
+    const matterportPresence = new Presence3D(mpSdk, {
+      isAvatarsEnabled: true,
+      isLaserEnabled: true,
+      isNameEnabled: true,
+      avatarConfig: {
+        height: 0,
+        scale: 1,
+        laserOrigin: { x: 0, y: 0, z: 0 },
+      },
+    });
+
+    room.addComponent(matterportPresence as any);
+  };
+
+  const enter = async () => {
     const showcaseWindow = ref.current.contentWindow;
 
-    if(!showcaseWindow) return;
-    const mpSdk = await showcaseWindow.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY);    
-    initSuperVizWithMatterport(roomId, userId, name, avatar, mpSdk);
+    if (!showcaseWindow) return;
+    const mpSdk = await showcaseWindow.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY);
+
+    initSuperVizWithMatterport(mpSdk);
     setDisableButton(true);
-  }
+  };
 
   return (
     <>
-    <section>
-      <button onClick={enter} disabled={disableButton}>Join Matterport room as "{name}"</button>
-      <iframe ref={ref} id={containerId} src={`/mp-bundle/showcase.html?&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=${modelId}`}/>
-    </section>
+      <section>
+        <button onClick={enter} disabled={disableButton}>
+          Join Matterport room as "{name}"
+        </button>
+        <iframe
+          ref={ref}
+          id={name + `-container`}
+          src={`/mp-bundle/showcase.html?&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=${modelId}`}
+        />
+      </section>
     </>
   );
 }
