c7227cb29df25b87cec2bc56fe884a36a390c391
Ian Silva <raspincel@gmail.com>
2023-11-28 09:38:48 -0300

    feat: create samples for who is online with vue

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ function App() {
   // We are initializing multiple rooms for demo purposes.
   return (
     <main>
-      <WhoIsOnlineContainer name="Zeus" roomId={roomId} />
-      <WhoIsOnlineContainer name="Hera" roomId={roomId} />
-      <WhoIsOnlineContainer name="Apollo" roomId={roomId} />
-      <WhoIsOnlineContainer name="Artemis" roomId={roomId} />
-      <WhoIsOnlineContainer name="Dionysus" roomId={roomId} />
-      <WhoIsOnlineContainer name="Athena" roomId={roomId} />
+        <WhoIsOnlineContainer name="Zeus" roomId={roomId} />
+        <WhoIsOnlineContainer name="Hera" roomId={roomId} />
+        <WhoIsOnlineContainer name="Apollo" roomId={roomId} />
+        <WhoIsOnlineContainer name="Artemis" roomId={roomId} />
+        <WhoIsOnlineContainer name="Dionysus" roomId={roomId} />
+        <WhoIsOnlineContainer name="Athena" roomId={roomId} />
     </main>
   );
 }
