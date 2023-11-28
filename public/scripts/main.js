b41ebdca49babb1f2f91bb96252b765a5272239a
Vitor Norton <vtnorton@outlook.com>
2023-11-28 19:57:40 -0300

    Comments with HTML

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,21 +4,27 @@ const roomId = "efd689fe-03b0-442e-ba4e-fb0bbd39dfe7";
 const groupId = "sv-sample-room-cdn-js-contextual-comments-html";
 const groupName = "Sample Room for Contextual Comments for HTML (CDN/JS)";
 
+let room;
+let participantName = "Zeus";
+
 document.addEventListener("DOMContentLoaded", function () {
-  InitSuperVizComments();
+  document.getElementById("participant-name").innerHTML = "View from " + participantName + " participant";
+  InitSuperVizRoomWithComments(participantName);
 });
 
-function InitSuperVizComments() {
-  // We are initializing two rooms for demo propose.
-  InitSuperVizRoomWithComments("Zeus");
-  InitSuperVizRoomWithComments("Hera");
+document.getElementById("change-participant").addEventListener("click", changeParticipant);
+
+function changeParticipant() {
+  participantName = participantName == "Zeus" ? "Hera" : "Zeus";
+  document.getElementById("participant-name").innerHTML = "View from " + participantName + " participant";
+
+  room.destroy();
+
+  InitSuperVizRoomWithComments(participantName);
 }
 
 async function InitSuperVizRoomWithComments(participant) {
-  // We are using the participant name to allocate the comments modal on their side of the screen. Only for demonstration purpose.
-  const position = participant == "Zeus" ? "left" : "right";
-
-  const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
+  room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
     roomId: roomId,
     group: {
       id: groupId,
@@ -31,10 +37,10 @@ async function InitSuperVizRoomWithComments(participant) {
     environment: "dev",
   });
 
-  const pinAdapter = new window.SuperVizRoom.CanvasPin(`${participant.toLowerCase()}-participant`);
+  const pinAdapter = new window.SuperVizRoom.CanvasPin(`participant-canvas`);
   const comments = new window.SuperVizRoom.Comments(pinAdapter, {
-    position: position,
-    buttonLocation: `top-${position}`,
+    position: "left",
+    buttonLocation: `top-left`,
   });
 
   room.addComponent(comments);
