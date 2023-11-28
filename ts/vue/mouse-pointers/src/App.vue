842c81522cfe2fdbd36ad8a20ed4d64fc99afc8b
Ian Silva <raspincel@gmail.com>
2023-11-28 08:24:39 -0300

    feat: create sample of mouse pointers using vue

---
+++ ts/vue/mouse-pointers/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <mouse-canvas participantName="Zeus" :roomId="roomId"/>
+  <mouse-canvas participantName="Hera" :roomId="roomId"/>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+import MouseCanvas from './components/MouseCanvas.vue';
+
+import { v4 as uuidv4 } from "uuid";
+
+
+export default defineComponent({
+  name: 'App',
+  components: { MouseCanvas }, 
+  setup() {
+    const roomId = ref(uuidv4()).value;
+
+    return {
+      roomId,
+    };
+  }
+});
+</script>
