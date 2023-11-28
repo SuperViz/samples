69f1ee28c289e373ca535cbfa9d13000bf18c40e
Vitor Norton <vtnorton@outlook.com>
2023-11-28 08:17:19 -0300

    Comments ThreeJS and Comments Matterport

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import "./index.css"
-import CommentsInstance from "./components/CommentsInstance"
-import { useState } from "react"
+import "./index.css";
+import CommentsInstance from "./components/CommentsInstance";
+import { useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const roomId = useRef(uuidv4()).current;
+
   const [showZeus, setShowZeus] = useState(true);
 
   const toggle = () => {
     setShowZeus(!showZeus);
   };
 
-  const props = {
-    name: showZeus ? "Zeus" : "Hera",
-    position: showZeus ? "right" : "left",
-  } as { name: string; position: "left" | "right" }
-
-  return <CommentsInstance {...props} toggle={toggle} key={String(showZeus)} />;
+  // We are initializing multiple rooms for demo purposes.
+  return (
+    <main>
+      {showZeus && <CommentsInstance name="Zeus" toggle={toggle} roomId={roomId} />}
+      {!showZeus && <CommentsInstance name="Hera" toggle={toggle} roomId={roomId} />}
+    </main>
+  );
 }
 
 export default App;
