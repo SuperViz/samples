0170144935f222ed68bf43f7ad956cacddda94c3
Vitor Norton <vtnorton@outlook.com>
2023-11-28 19:52:41 -0300

    Comments with Matterport

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,31 @@
 import { DEVELOPER_KEY, MATTERPORT_KEY } from "../env.js";
 
-const roomId = "AAd689fe-03b0-442f-ba5e-fb0bbd39d983";
+const roomId = "55ec6752-98e8-48c6-b1b6-74bf736c6b14";
 const groupId = "sv-sample-room-cdn-js-contextual-comments-matterport";
 const groupName = "Sample Room for Contextual Comments with Matterport (CDN/JS)";
 const modelId = "LmRnZAsWoxy";
 
+let room;
+let participantName = "Zeus";
+
 document.addEventListener("DOMContentLoaded", function () {
-  InitMatterportIntegrationWithSuperViz();
+  document.getElementById("participant-name").innerHTML = "View from " + participantName + " participant";
+  InitParticipantMatterport(participantName);
 });
 
-function InitMatterportIntegrationWithSuperViz() {
-  // We are initializing two rooms for demo propose.
-  InitFirstParticipantMatterport();
-  InitSecondParticipantMatterport();
+document.getElementById("change-participant").addEventListener("click", changeParticipant);
+
+function changeParticipant() {
+  participantName = participantName == "Zeus" ? "Hera" : "Zeus";
+  document.getElementById("participant-name").innerHTML = "View from " + participantName + " participant";
+
+  room.destroy();
+
+  InitParticipantMatterport(participantName);
 }
 
-function InitFirstParticipantMatterport() {
-  const showcase = document.getElementById("zeus-participant");
+function InitParticipantMatterport(participantName) {
+  const showcase = document.getElementById("participant-showcase");
   if (!showcase) return;
 
   const showcaseWindow = showcase.contentWindow;
@@ -26,30 +35,12 @@ function InitFirstParticipantMatterport() {
     if (!showcaseWindow) return;
     const mpSDK = await showcaseWindow.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY);
 
-    InitSuperVizRoomWithMatterport(mpSDK, showcase, "Zeus");
-  });
-}
-
-function InitSecondParticipantMatterport() {
-  const showcase = document.getElementById("hera-participant");
-  if (!showcase) return;
-
-  const showcaseWindow = showcase.contentWindow;
-  showcase.src = `./mp-bundle/showcase.html?&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=${modelId}`;
-
-  showcase.addEventListener("load", async () => {
-    if (!showcaseWindow) return;
-    const mpSDK = await showcaseWindow.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY);
-
-    InitSuperVizRoomWithMatterport(mpSDK, showcase, "Hera");
+    InitSuperVizRoomWithMatterport(mpSDK, showcase, participantName);
   });
 }
 
 async function InitSuperVizRoomWithMatterport(mpSDK, showcase, participant) {
-  // We are using the participant name to allocate the comments modal on their side of the screen. Only for demonstration purpose.
-  const position = participant == "Zeus" ? "left" : "right";
-
-  const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
+  room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
     roomId: roomId,
     group: {
       id: groupId,
@@ -64,8 +55,8 @@ async function InitSuperVizRoomWithMatterport(mpSDK, showcase, participant) {
 
   const pinAdapter = new window.MatterportPin(mpSDK, showcase);
   const comments = new window.SuperVizRoom.Comments(pinAdapter, {
-    position: position,
-    buttonLocation: `top-${position}`,
+    position: "left",
+    buttonLocation: `top-left`,
   });
 
   room.addComponent(comments);
