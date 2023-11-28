3da9b9ea2a50d3bcf23a9a8947d168d18702bba5
Ian Silva <raspincel@gmail.com>
2023-11-28 08:24:39 -0300

    feat: create sample of mouse pointers using vue

---
+++ src/components/MouseCanvas.vue
@@ -0,0 +1,68 @@
+<template>
+  <section>
+    <h1>View from "{{ participantName }}" participant</h1>
+    <canvas :id="canvasId" />
+  </section>
+</template>
+  
+<script lang="ts">
+  import SuperVizRoom from "@superviz/sdk";
+  import { MousePointers } from "@superviz/sdk/lib/components";
+  
+  import { ref, onMounted, defineComponent } from "vue";
+  
+  const groupId = "sv-sample-room-cdn-mouse-pointers";
+  const groupName = "Sample Room for Mouse Pointers (Vue TS)";
+
+  const DEVELOPER_KEY = process.env.VUE_APP_SUPERVIZ_DEVELOPER_TOKEN as string;
+  ''
+  export default defineComponent({
+    name: 'MouseCanvas',
+    props: {
+      participantName: {
+        type: String,
+        required: true,
+      },
+      roomId: {
+        type: String,
+        required: true,
+      },
+    },
+    setup(props) {
+      const canvasId = ref(props.participantName + "-canvas");
+      const userId = ref(props.participantName.toLowerCase());
+      const loaded = ref(false);
+      
+      async function InitSuperVizMousePointers() {
+        const room = await SuperVizRoom(DEVELOPER_KEY, {
+          roomId: props.roomId,
+          group: {
+            id: groupId,
+            name: groupName,
+          },
+          participant: {
+            id: userId.value,
+            name: props.participantName,
+          },
+          environment: "dev",
+        });
+  
+        const mousePointer = new MousePointers(canvasId.value);
+        room.addComponent(mousePointer);
+      }
+  
+      onMounted(async () => {
+        if (loaded.value) return;
+        await InitSuperVizMousePointers();
+        
+        return () => {
+          loaded.value = true;
+        };
+      });
+  
+      return {
+        canvasId,
+      };
+    },
+  });
+  </script>
