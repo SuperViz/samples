6ab2190e73a28f69d2181ddfb28bf3bd63868e62
Ian Silva <raspincel@gmail.com>
2024-02-16 11:44:43 -0300

    feat: use sdk-react for who is online (js)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { useRef } from "react";
-import WhoIsOnlineContainer from "./components/WhoIsOnlineContainer";
-import { v4 as uuidv4 } from "uuid";
+import { SuperVizRoomProvider } from "@superviz/react-sdk";
+import Room from "./components/Room";
+
+const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
+const groupId = "sv-sample-room-react-js-who-is-online";
+const groupName = "Sample Room for Who-is-Online (React/JS)";
 
 function App() {
-  const roomId = useRef(uuidv4()).current;
+  const roomId = 'samples-who-is-online-room';
+  const user = Math.floor(Math.random() * 100);
 
   // We are initializing multiple rooms for demo purposes.
   return (
     <main>
-      <WhoIsOnlineContainer name="Zeus" roomId={roomId} />
-      <WhoIsOnlineContainer name="Hera" roomId={roomId} />
-      <WhoIsOnlineContainer name="Apollo" roomId={roomId} />
-      <WhoIsOnlineContainer name="Artemis" roomId={roomId} />
-      <WhoIsOnlineContainer name="Dionysus" roomId={roomId} />
-      <WhoIsOnlineContainer name="Athena" roomId={roomId} />
+      <SuperVizRoomProvider
+        developerKey={DEVELOPER_KEY}
+        debug={true}
+        group={{
+          id: groupId,
+          name: groupName,
+        }}
+        participant={{
+          id: user.toString(),
+          name: "John " + user,
+        }}
+        roomId={roomId}
+      >
+        <Room />
+      </SuperVizRoomProvider>
     </main>
   );
 }
