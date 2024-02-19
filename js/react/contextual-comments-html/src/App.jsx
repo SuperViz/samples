29dd34e00fc51b88e0280eaa595b242a44fef1db
Ian Silva <raspincel@gmail.com>
2024-02-19 10:38:20 -0300

    feat: update comments html react sample to the same of the doc-samples with the react sdk

--- js/react/contextual-comments-html/src/App.jsx
+++ js/react/contextual-comments-html/src/App.jsx
@@ -1,23 +1,34 @@
-import "./index.css";
-import CommentsInstance from "./components/CommentsInstance";
-import { useRef, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { SuperVizRoomProvider } from "@superviz/react-sdk";
+
+import Room from "./components/Room.tsx";
+import { useState } from "react";
+
+const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
+const user = Math.floor(Math.random() * 10);
+const groupId = "sv-sample-room-react-js-presence-autodesk";
+const groupName = "Sample Room for Presence Autodesk (React/JS)";
+const roomId = 'samples-presence-autodesk-room';
 
 function App() {
-  const roomId = useRef(uuidv4()).current;
+  const [loaded, setLoaded] = useState(false);
 
-  const [showZeus, setShowZeus] = useState(true);
-
-  const toggle = () => {
-    setShowZeus(!showZeus);
-  };
-
-  // We are initializing multiple rooms for demo purposes.
   return (
-    <main>
-      {showZeus && <CommentsInstance name="Zeus" toggle={toggle} roomId={roomId} />}
-      {!showZeus && <CommentsInstance name="Hera" toggle={toggle} roomId={roomId} />}
-    </main>
+    <SuperVizRoomProvider
+      developerKey={DEVELOPER_KEY}
+      debug={true}
+      group={{
+        id: groupId,
+        name: groupName,
+      }}
+      participant={{
+        id: user.toString(),
+        name: "John " + user,
+      }}
+      roomId={roomId}
+      onParticipantJoined={()=> setLoaded(true)}
+    >
+      <Room loaded={loaded}/>
+    </SuperVizRoomProvider>
   );
 }
 
