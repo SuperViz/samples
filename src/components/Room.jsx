c0b8d360ed30267d68b4bea77daaec6561b07e88
Ian Silva <raspincel@gmail.com>
2024-02-20 07:21:51 -0300

    feat: update react samples with typescript so they use our react sdk

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -2,17 +2,17 @@ import { WhoIsOnline, useSuperVizRoom } from "@superviz/react-sdk";
 import { useEffect } from "react";
 
 export default function Room() {
-  const { startRoom, stopRoom, isJoinedRoom } = useSuperVizRoom();
+  const { startRoom, stopRoom, hasJoinedRoom } = useSuperVizRoom();
 
   // This effect will start the room when the component is mounted
   // and stop the room when the component is unmounted
   useEffect(() => {
-    if (!startRoom || isJoinedRoom) return;
+    if (!startRoom || hasJoinedRoom) return;
 
     startRoom();
 
     return () => {
-      if (!stopRoom || !isJoinedRoom) return;
+      if (!stopRoom || !hasJoinedRoom) return;
 
       stopRoom();
     };
@@ -27,48 +27,3 @@ export default function Room() {
     </section>
   );
 }
-
-/**
- * import { WhoIsOnline, useSuperVizRoom } from "@superviz/react-sdk";
-import { useEffect } from "react";
-
-function Room() {
-  const { startRoom, stopRoom, isJoinedRoom } = useSuperVizRoom();
-
-  // This effect will start the room when the component is mounted
-  // and stop the room when the component is unmounted
-  useEffect(() => {
-    if (!startRoom || isJoinedRoom) return;
-
-    startRoom();
-
-    return () => {
-      if (!stopRoom || !isJoinedRoom) return;
-
-      stopRoom();
-    };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const styles = `
-  .who-is-online__participant {
-
-    border-width:5px;
-  }
-
-  .who-is-online__participant img {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-  return (
-    <>
-      <WhoIsOnline styles={styles} />
-    </>
-  );
-}
-
-export default Room;
- */
