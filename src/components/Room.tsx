e6047389aea0ea85dba425b3e2668fdf1f56d901
Ian Silva <raspincel@gmail.com>
2024-02-20 07:21:51 -0300

    feat: update react samples with typescript so they use our react sdk

---
+++ src/components/Room.tsx
@@ -0,0 +1,33 @@
+import { useSuperVizRoom, VideoConference } from "@superviz/react-sdk";
+import { useEffect } from "react";
+
+function Room() {
+  const { startRoom, stopRoom, hasJoinedRoom } = useSuperVizRoom();
+
+  const collabMode = {
+    enabled: false,
+  };
+
+  // This effect will start the room when the component is mounted
+  // and stop the room when the component is unmounted
+  useEffect(() => {
+    if (!startRoom || hasJoinedRoom) return;
+
+    startRoom();
+
+    return () => {
+      if (!stopRoom || !hasJoinedRoom) return;
+
+      stopRoom();
+    };
+  }, []);
+
+  return (
+    <VideoConference
+      participantType="host"
+      collaborationMode={collabMode}
+    ></VideoConference>
+  );
+}
+
+export default Room;
