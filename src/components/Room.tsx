5a26ebb29d55e46218ccd9f309da0749dae029d3
Ian Silva <raspincel@gmail.com>
2024-02-21 08:09:58 -0300

    fix: move canvas element to App in mouse pointers

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,12 +1,7 @@
 import { MousePointers } from "@superviz/react-sdk";
 
 function Room() {
-  return (
-    <>
-      <MousePointers elementId="element-id" />
-      <canvas id="element-id"></canvas>
-    </>
-  );
+  return <MousePointers elementId="element-id" />
 }
 
 export default Room;
