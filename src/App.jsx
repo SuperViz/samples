9d44b915b1681f6fee2f80cadb59e0e3900bc35e
Ian Silva <raspincel@gmail.com>
2024-02-21 11:21:13 -0300

    feat: create contextual comments canvas js sample

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { SuperVizRoomProvider } from "@superviz/react-sdk";
+
+import Room from "./components/Room";
+
+const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
+const user = Math.floor(Math.random() * 10);
+const groupId = "sv-sample-room-react-ts-comments-canvas";
+const groupName = "Sample Room for Presence Autodesk (React/TS)";
+const roomId = 'samples-presence-autodesk-room';
+
+function App() {
+  return (
+    <SuperVizRoomProvider
+      developerKey={DEVELOPER_KEY}
+      debug={true}
+      group={{
+        id: groupId,
+        name: groupName,
+      }}
+      participant={{
+        id: user.toString(),
+        name: "John " + user,
+      }}
+      roomId={roomId}
+    >
+      <Room />
+    </SuperVizRoomProvider>
+  );
+}
+
+export default App;
