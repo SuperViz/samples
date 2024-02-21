4664fbd2943082866225fc09c30209bf44b3a88e
Ian Silva <raspincel@gmail.com>
2024-02-21 08:04:25 -0300

    feat: update samples and their versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import Room from './components/Room'
 import { SuperVizRoomProvider } from "@superviz/react-sdk";
 
+import Room from "./components/Room";
+
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
-const groupId = "sv-sample-room-react-mouse-pointers";
-const groupName = "Sample Room for Mouse Pointers (React + TS)";
-const roomId = "samples-mouse-pointers-room";
+const groupId = "sv-sample-room-react-ts-mouse-pointers";
+const groupName = "Sample Room for Mouse Pointers (React/TS)";
+const roomId = 'samples-mouse-pointers-room';
 const user = Math.floor(Math.random() * 100);
 
 function App() {
   return (
     <SuperVizRoomProvider
       developerKey={DEVELOPER_KEY}
+      debug={true}
       group={{
         id: groupId,
         name: groupName,
