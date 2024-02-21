07e81d57bbcf7511dda3457f49686bf44b40a7fc
Ian Silva <raspincel@gmail.com>
2024-02-21 08:04:25 -0300

    feat: update samples and their versions

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { SuperVizRoomProvider } from "@superviz/react-sdk";
+
+import Room from "./components/Room.tsx";
+import { useState } from "react";
+
+const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
+const user = Math.floor(Math.random() * 10);
+const groupId = "sv-sample-room-react-ts-comments-canvas";
+const groupName = "Sample Room for Presence Autodesk (React/TS)";
+const roomId = 'samples-presence-autodesk-room';
+
+function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <SuperVizRoomProvider
+      developerKey={DEVELOPER_KEY}
+      debug={true}
+      group={{
+        id: groupId,
+        name: groupName,
+      }}
+      participant={{
+        id: user.toString(),
+        name: "John " + user,
+      }}
+      roomId={roomId}
+      onParticipantJoined={()=> setLoaded(true)}
+    >
+      <Room loaded={loaded}/>
+    </SuperVizRoomProvider>
+  );
+}
+
+export default App;
