b06992c0ffd4fcb409da25c630174147ab04693f
Ian Silva <raspincel@gmail.com>
2024-02-21 08:04:25 -0300

    feat: update samples and their versions

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { useRef, useState } from "react";
-import ThreeJSContainer from "./components/CommentsContainer";
-import { v4 as uuidv4 } from "uuid";
+import { SuperVizRoomProvider } from "@superviz/react-sdk";
+
+import Room from "./components/Room";
+
+const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
+const groupId = "sv-sample-room-react-ts-threejs-contextual-comments";
+const groupName = "Sample Room for ThreeJs Contextual Comments (React/TS)";
+const roomId = 'samples-threejs-contextual-comments-room';
+const user = Math.floor(Math.random() * 100);
 
 function App() {
-  const roomId = useRef(uuidv4()).current;
-  const [showZeus, setShowZeus] = useState(true);
-
-  const toggle = () => {
-    setShowZeus(!showZeus);
-  };
-
-  // We are initializing multiple rooms for demo purposes.
   return (
-    <main>
-      {showZeus && <ThreeJSContainer name="Zeus" toggle={toggle} roomId={roomId} />}
-      {!showZeus && <ThreeJSContainer name="Hera" toggle={toggle} roomId={roomId} />}
-    </main>
+    <SuperVizRoomProvider
+      developerKey={DEVELOPER_KEY}
+      debug={true}
+      group={{
+        id: groupId,
+        name: groupName,
+      }}
+      participant={{
+        id: user.toString(),
+        name: "John " + user,
+      }}
+      roomId={roomId}
+
+    >
+      <Room />
+    </SuperVizRoomProvider>
   );
 }
 
