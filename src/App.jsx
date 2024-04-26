5559e8c1852d332def73fc3dd71af3fe93a66a42
Vitor Norton <vtnorton@outlook.com>
2024-04-26 16:15:59 -0300

    js/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
 import { SuperVizRoomProvider } from "@superviz/react-sdk";
-
-import Room from "./components/Room";
+import CanvasCommentsImplementation from "./components/CanvasCommentsImplementation";
 
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
 const user = Math.floor(Math.random() * 10);
-const groupId = "sv-sample-room-react-ts-comments-canvas";
-const groupName = "Sample Room for Presence Autodesk (React/TS)";
-const roomId = 'samples-presence-autodesk-room';
+const groupId = "sv-sample-room-react-js-comments-canvas";
+const groupName = "Sample Room for Contextual Comments Canvas (React/JS)";
 
 function App() {
-  return (
-    <SuperVizRoomProvider
-      developerKey={DEVELOPER_KEY}
-      debug={true}
-      group={{
-        id: groupId,
-        name: groupName,
-      }}
-      participant={{
-        id: user.toString(),
-        name: "John " + user,
-      }}
-      roomId={roomId}
-    >
-      <Room />
-    </SuperVizRoomProvider>
-  );
+    return (
+        <SuperVizRoomProvider
+            developerKey={DEVELOPER_KEY}
+            group={{
+                id: groupId,
+                name: groupName,
+            }}
+            participant={{
+                id: user.toString(),
+                name: "John " + user,
+            }}
+            roomId={groupId}
+        >
+            <CanvasCommentsImplementation />
+        </SuperVizRoomProvider>
+    );
 }
 
 export default App;
