43d097d3449406124fe66cc8971e7fb50f2567d5
Vitor Norton <vtnorton@outlook.com>
2024-04-26 16:15:59 -0300

    js/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import { SuperVizRoomProvider } from "@superviz/react-sdk";
 
-import Room from "./components/Room";
+import ContextualCommentsThreeJSImplementation from "./components/ContextualCommentsThreeJSImplementation";
 
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
-const groupId = "sv-sample-room-react-ts-threejs-contextual-comments";
-const groupName = "Sample Room for ThreeJs Contextual Comments (React/TS)";
-const roomId = 'samples-threejs-contextual-comments-room';
+const groupId = "sv-sample-room-react-js-threejs-contextual-comments";
+const groupName = "Sample Room for ThreeJs Contextual Comments (React/JS)";
 const user = Math.floor(Math.random() * 100);
 
 function App() {
-  return (
-    <SuperVizRoomProvider
-      developerKey={DEVELOPER_KEY}
-      debug={true}
-      group={{
-        id: groupId,
-        name: groupName,
-      }}
-      participant={{
-        id: user.toString(),
-        name: "John " + user,
-      }}
-      roomId={roomId}
-    >
-      <Room />
-      <canvas id='threejs-canvas' />
-
-    </SuperVizRoomProvider>
-  );
+    return (
+        <SuperVizRoomProvider
+            developerKey={DEVELOPER_KEY}
+            group={{
+                id: groupId,
+                name: groupName,
+            }}
+            participant={{
+                id: user.toString(),
+                name: "John " + user,
+            }}
+            roomId={groupId}
+        >
+            <ContextualCommentsThreeJSImplementation />
+        </SuperVizRoomProvider>
+    );
 }
-
 
 export default App;
