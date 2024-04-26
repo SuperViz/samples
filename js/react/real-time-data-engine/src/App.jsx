a44a3a894091070cbc6192c268c9cf592fc1d8b8
Vitor Norton <vtnorton@outlook.com>
2024-04-26 16:32:41 -0300

    Fixes for Real-time

--- js/react/real-time-data-engine/src/App.jsx
+++ js/react/real-time-data-engine/src/App.jsx
@@ -1,6 +1,6 @@
 import { Realtime, SuperVizRoomProvider } from "@superviz/react-sdk";
 
-import EventForm from "./components/EventForm.jsx";
+import EventForm from "./components/EventForm";
 
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
 const user = Math.floor(Math.random() * 10);
@@ -8,25 +8,25 @@ const groupId = "sv-sample-room-react-js-real-time-data-engine";
 const groupName = "Sample Room for Real-time Data Engine (React/JS)";
 
 function App() {
-    return (
-        <main>
-            <SuperVizRoomProvider
-                developerKey={DEVELOPER_KEY}
-                group={{
-                    id: groupId,
-                    name: groupName,
-                }}
-                participant={{
-                    id: user.toString(),
-                    name: "John" + user,
-                }}
-                roomId={groupId}
-            >
-                <Realtime />
-                <EventForm participantId={user.toString()} />
-            </SuperVizRoomProvider>
-        </main>
-    );
+  return (
+    <main>
+      <SuperVizRoomProvider
+        developerKey={DEVELOPER_KEY}
+        group={{
+          id: groupId,
+          name: groupName,
+        }}
+        participant={{
+          id: user.toString(),
+          name: "John" + user,
+        }}
+        roomId={groupId}
+      >
+        <Realtime />
+        <EventForm participantId={user.toString()} />
+      </SuperVizRoomProvider>
+    </main>
+  );
 }
 
 export default App;
