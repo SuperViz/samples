f13df5218a248413915fd7b4ed503b707b4db12a
Vitor Norton <vtnorton@outlook.com>
2024-05-03 22:14:25 -0300

    MP projects

--- ts/react/matterport/src/App.tsx
+++ ts/react/matterport/src/App.tsx
@@ -1,7 +1,7 @@
 import { MatterportIframe, SuperVizRoomProvider } from "@superviz/react-sdk";
 
 const modelId = "7ffnfBNamei";
-const matterportKey = import.meta.env.VITE_MATTERPORT_KEY;
+const MATTERPORT_KEY = import.meta.env.VITE_MATTERPORT_KEY;
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
 const groupId = "sv-sample-room-react-ts-presence-matterport";
 const groupName = "Sample Room for Presence Matterport (React/TS)";
@@ -25,8 +25,8 @@ function App() {
         <MatterportIframe
           width={window.innerWidth}
           height={window.innerHeight}
-          bundleUrl={`/mp-bundle/showcase.html?&brand=0&mls=2&mt=0&search=0&kb=0&play=1&qs=1&applicationKey=${matterportKey}&m=${modelId}`}
-          matterportKey={matterportKey}
+          bundleUrl={`/mp-bundle/showcase.html?&brand=0&mls=2&mt=0&search=0&kb=0&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=${modelId}`}
+          MATTERPORT_KEY={MATTERPORT_KEY}
         />
       </section>
     </SuperVizRoomProvider>
