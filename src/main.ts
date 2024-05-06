6a5bb74602b2d351deb089c43e25651a999143b9
vítor norton <vtnorton@outlook.com>
2024-05-06 18:16:31 -0300

    Update with v6 (#7)
    * add projects
    
    * update title
    
    * MP projects
    
    * canvas comment
    
    * real-time vanilla ts
    
    * js-vanilla part 1
    
    * wio vanilla js
    
    * video
    
    * three
    
    * realtime
    
    * mouse
    
    * mp
    
    * comments three
    
    * comments matterport
    
    * html canvas
    
    * canvas comments
    
    * autodesk comments
    
    * autodesk
    
    * update readme
    
    * changed user to participant
    
    * Update naming

---
+++ src/main.ts
@@ -0,0 +1,88 @@
+import { ThreeJsPin } from "@superviz/threejs-plugin";
+import "./style.css";
+import SuperVizRoom, { Comments } from "@superviz/sdk";
+import * as THREE from "three";
+import { sampleInfo } from "./projectInfo";
+
+import { RoomEnvironment } from "three/examples/jsm/environments/RoomEnvironment.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
+const participant = Math.floor(Math.random() * 100);
+const groupId = sampleInfo.id;
+const groupName = sampleInfo.name;
+
+function InitParticipantThreeJS() {
+  const container = document.getElementById("participant-canvas")!;
+  const width = container.clientWidth;
+  const height = container.clientHeight;
+
+  const renderer = new THREE.WebGLRenderer({ canvas: container, antialias: true });
+  renderer.setSize(width, height);
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xb6b7b8);
+  scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
+  const camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 300);
+  camera.position.set(2, 0, 2);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0.5, 0);
+  controls.update();
+  controls.enablePan = false;
+  controls.enableDamping = true;
+
+  const loader = new GLTFLoader();
+  loader.load(
+    "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/GlamVelvetSofa/glTF-Binary/GlamVelvetSofa.glb",
+    function (gltf: any) {
+      scene.add(gltf.scene);
+      InitSuperVizRoomWithThreeJS(scene, renderer, camera);
+      animate();
+    },
+    undefined,
+    function (e: any) {
+      console.error(e);
+    }
+  );
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    controls.update();
+
+    renderer.render(scene, camera);
+  };
+
+  animate();
+}
+
+async function InitSuperVizRoomWithThreeJS(
+  scene: THREE.Scene,
+  renderer: THREE.WebGLRenderer,
+  camera: THREE.PerspectiveCamera
+) {
+  const room = await SuperVizRoom(DEVELOPER_KEY, {
+    roomId: groupId,
+    group: {
+      id: groupId,
+      name: groupName,
+    },
+    participant: {
+      id: participant.toString(),
+      name: "John " + participant,
+      avatar: {
+        imageUrl: `https://production.cdn.superviz.com/static/default-avatars/2.png`,
+        model3DUrl: `https://production.storage.superviz.com/readyplayerme/2.glb`,
+      },
+    },
+  });
+
+  const pinAdapter = new ThreeJsPin(scene, renderer, camera);
+  const comments = new Comments(pinAdapter as any);
+
+  room.addComponent(comments);
+}
+
+InitParticipantThreeJS();
