f19043ad4ced7db334e43670b6e2fe243d274aba
vítor norton <vtnorton@outlook.com>
2024-05-06 18:16:31 -0300

    Update with v6 (#7)
    * add projects
    
    * update title
    
    * MP projects
    
    * canvas comment
    
    * real-time vanilla ts
    
    * js-vanilla part 1
    
    * wio vanilla js
    
    * video
    
    * three
    
    * realtime
    
    * mouse
    
    * mp
    
    * comments three
    
    * comments matterport
    
    * html canvas
    
    * canvas comments
    
    * autodesk comments
    
    * autodesk
    
    * update readme
    
    * changed user to participant
    
    * Update naming

--- js/cdn/autodesk/public/env.example.js
+++
@@ -1,3 +0,0 @@
-export const CLIENT_ID = "";
-export const CLIENT_SECRET = "";
-export const DEVELOPER_KEY = "";
--- ts/react/contextual-comments-matterport/mp-bundle/js/143.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 143.js.LICENSE.txt */
-(self.webpackChunkmp_webgl=self.webpackChunkmp_webgl||[]).push([[143],{46102:()=>{}}]);
