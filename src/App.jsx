703839f025ba3d03a2f653091c32dd381666feff
vítor norton <vtnorton@outlook.com>
2024-05-06 18:16:31 -0300

    Update with v6 (#7)
    * add projects
    
    * update title
    
    * MP projects
    
    * canvas comment
    
    * real-time vanilla ts
    
    * js-vanilla part 1
    
    * wio vanilla js
    
    * video
    
    * three
    
    * realtime
    
    * mouse
    
    * mp
    
    * comments three
    
    * comments matterport
    
    * html canvas
    
    * canvas comments
    
    * autodesk comments
    
    * autodesk
    
    * update readme
    
    * changed user to participant
    
    * Update naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import { SuperVizRoomProvider } from "@superviz/react-sdk";
+import { sampleInfo } from "./projectInfo";
 
 import MatterportCommentsImplementation from "./components/MatterportCommentsImplementation";
 
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY;
-const groupId = "sv-sample-room-react-js-autodesk-contextual-comments";
-const groupName = "Sample Room for Matterport Contextual Comments (React/JS)";
-const user = Math.floor(Math.random() * 100);
+const groupId = sampleInfo.id;
+const groupName = sampleInfo.name;
+const participant = Math.floor(Math.random() * 100);
 
 function App() {
-    return (
-        <SuperVizRoomProvider
-            developerKey={DEVELOPER_KEY}
-            group={{
-                id: groupId,
-                name: groupName,
-            }}
-            participant={{
-                id: user.toString(),
-                name: "John " + user,
-            }}
-            roomId={groupId}
-        >
-            <MatterportCommentsImplementation />
-        </SuperVizRoomProvider>
-    );
+  return (
+    <SuperVizRoomProvider
+      developerKey={DEVELOPER_KEY}
+      group={{
+        id: groupId,
+        name: groupName,
+      }}
+      participant={{
+        id: participant.toString(),
+        name: "John " + participant,
+      }}
+      roomId={groupId}
+    >
+      <MatterportCommentsImplementation />
+    </SuperVizRoomProvider>
+  );
 }
 
 export default App;
