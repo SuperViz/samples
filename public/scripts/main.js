9d6463a7c0b21b5b612e9c03cfd14d8f2b43978b
vítor norton <vtnorton@outlook.com>
2024-05-14 14:48:43 -0300

    Update CDN Projects (#8)
    * update scripts
    
    * testing script
    
    * update permission
    
    * update working-directory
    
    * Raised quality bar
    
    * improved performance
    
    * update script
    
    * update error code
    
    * Raised the bar
    
    * Update conditional
    
    * Fixed type error
    
    * Update beta
    
    * cdn autodesk
    
    * cdn autodesk comments
    
    * comments canvas
    
    * html comments
    
    * matterport comments
    
    * threejs comments
    
    * cdn matterport
    
    * cdn mouse
    
    * cdn real time
    
    * cdn three
    
    * video cdn
    
    * cdn wio
    
    * Update CDN Projects

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,47 +1,25 @@
 import { DEVELOPER_KEY } from "../env.js";
-import { sampleInfo } from "../projectInfo";
+import { sampleInfo } from "../projectInfo.js";
 
-let roomId;
+const participant = Math.floor(Math.random() * 100);
 const groupId = sampleInfo.id;
 const groupName = sampleInfo.name;
 
-document.addEventListener("DOMContentLoaded", function () {
-  roomId = generateUUID();
-});
-
-document.getElementById("enter-call").addEventListener("click", initSuperVizWithVideoConference);
-
-async function initSuperVizWithVideoConference() {
+async function initializeSuperVizRoom() {
   const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
-    roomId: roomId,
+    roomId: groupId,
     group: {
       id: groupId,
       name: groupName,
     },
     participant: {
-      id: "zeus",
-      name: "Zeus",
+      id: participant.toString(),
+      name: "John " + participant,
     },
   });
-
-  const videoConference = new window.SuperVizRoom.VideoConference({
-    participantType: "host",
-  });
-  room.addComponent(videoConference);
+  const video = new window.SuperVizRoom.VideoConference({ participantType: "host" });
+  room.addComponent(video);
+  return room;
 }
 
-function generateUUID() {
-  var d = new Date().getTime();
-  var d2 = (typeof performance !== "undefined" && performance.now && performance.now() * 1000) || 0; //Time in microseconds since page-load or 0 if unsupported
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = Math.random() * 16;
-    if (d > 0) {
-      r = (d + r) % 16 | 0;
-      d = Math.floor(d / 16);
-    } else {
-      r = (d2 + r) % 16 | 0;
-      d2 = Math.floor(d2 / 16);
-    }
-    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-  });
-}
+initializeSuperVizRoom();
