1b66fc3720f9e5fff2c538e9d137f55278ee956d
Vitor Norton <vtnorton@outlook.com>
2024-05-14 14:32:14 -0300

    cdn mouse

--- js/cdn/mouse-pointers/public/scripts/main.js
+++ js/cdn/mouse-pointers/public/scripts/main.js
@@ -1,70 +1,27 @@
 import { DEVELOPER_KEY } from "../env.js";
-import { sampleInfo } from "../projectInfo";
+import { sampleInfo } from "../projectInfo.js";
 
-const roomId = generateUUID();
+const participant = Math.floor(Math.random() * 100);
 const groupId = sampleInfo.id;
 const groupName = sampleInfo.name;
 
-document.addEventListener("DOMContentLoaded", function () {
-  InitSuperVizMousePointers();
-});
-
-function InitSuperVizMousePointers() {
-  // We are initializing two rooms for demo purposes.
-  InitFirstRoom();
-  InitSecondRoom();
-}
-
-async function InitFirstRoom() {
+async function initializeSuperVizRoom() {
   const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
-    roomId: roomId,
+    roomId: groupId,
     group: {
       id: groupId,
       name: groupName,
     },
     participant: {
-      id: "zeus",
-      name: "Zeus",
+      id: participant.toString(),
+      name: "John " + participant,
     },
   });
 
-  const mousePointers = new window.SuperVizRoom.MousePointers("mouse-pointer-1");
+  const mousePointers = new window.SuperVizRoom.MousePointers("element-id");
   room.addComponent(mousePointers);
 
   return room;
 }
 
-async function InitSecondRoom() {
-  const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
-    roomId: roomId,
-    group: {
-      id: groupId,
-      name: groupName,
-    },
-    participant: {
-      id: "hera",
-      name: "Hera",
-    },
-  });
-
-  const mousePointers = new window.SuperVizRoom.MousePointers("mouse-pointer-2");
-  room.addComponent(mousePointers);
-
-  return room;
-}
-
-function generateUUID() {
-  var d = new Date().getTime();
-  var d2 = (typeof performance !== "undefined" && performance.now && performance.now() * 1000) || 0; //Time in microseconds since page-load or 0 if unsupported
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = Math.random() * 16;
-    if (d > 0) {
-      r = (d + r) % 16 | 0;
-      d = Math.floor(d / 16);
-    } else {
-      r = (d2 + r) % 16 | 0;
-      d2 = Math.floor(d2 / 16);
-    }
-    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-  });
-}
+initializeSuperVizRoom();
