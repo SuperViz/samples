2586269dd88d45927fb6ca4274b429e192356bdd
vítor norton <vtnorton@outlook.com>
2024-05-14 14:48:43 -0300

    Update CDN Projects (#8)
    * update scripts
    
    * testing script
    
    * update permission
    
    * update working-directory
    
    * Raised quality bar
    
    * improved performance
    
    * update script
    
    * update error code
    
    * Raised the bar
    
    * Update conditional
    
    * Fixed type error
    
    * Update beta
    
    * cdn autodesk
    
    * cdn autodesk comments
    
    * comments canvas
    
    * html comments
    
    * matterport comments
    
    * threejs comments
    
    * cdn matterport
    
    * cdn mouse
    
    * cdn real time
    
    * cdn three
    
    * video cdn
    
    * cdn wio
    
    * Update CDN Projects

---
+++ public/scripts/main.js
@@ -0,0 +1,28 @@
+import { DEVELOPER_KEY } from "../env.js";
+import { sampleInfo } from "./../projectInfo.js";
+
+const participant = Math.floor(Math.random() * 100);
+const groupId = sampleInfo.id;
+const groupName = sampleInfo.name;
+
+async function initializeSuperVizRoom() {
+  const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
+    roomId: groupId,
+    group: {
+      id: groupId,
+      name: groupName,
+    },
+    participant: {
+      id: participant.toString(),
+      name: "John " + participant,
+    },
+  });
+
+  const pinAdapter = new window.SuperVizRoom.CanvasPin("element-id");
+  const comments = new window.SuperVizRoom.Comments(pinAdapter);
+  room.addComponent(comments);
+
+  return room;
+}
+
+initializeSuperVizRoom();
