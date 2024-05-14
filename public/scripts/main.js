7d2c3e3ecc69bce84e1d1601a196f1d67d55d97b
vítor norton <vtnorton@outlook.com>
2024-05-14 14:48:43 -0300

    Update CDN Projects (#8)
    * update scripts
    
    * testing script
    
    * update permission
    
    * update working-directory
    
    * Raised quality bar
    
    * improved performance
    
    * update script
    
    * update error code
    
    * Raised the bar
    
    * Update conditional
    
    * Fixed type error
    
    * Update beta
    
    * cdn autodesk
    
    * cdn autodesk comments
    
    * comments canvas
    
    * html comments
    
    * matterport comments
    
    * threejs comments
    
    * cdn matterport
    
    * cdn mouse
    
    * cdn real time
    
    * cdn three
    
    * video cdn
    
    * cdn wio
    
    * Update CDN Projects

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,46 +1,28 @@
 import { DEVELOPER_KEY } from "../env.js";
-import { sampleInfo } from "../projectInfo";
+import { sampleInfo } from "./../projectInfo.js";
 
-const roomId = "efd689fe-03b0-442e-ba4e-fb0bbd39dfe7";
+const participant = Math.floor(Math.random() * 100);
 const groupId = sampleInfo.id;
 const groupName = sampleInfo.name;
 
-let room;
-let participantName = "Zeus";
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("participant-name").innerHTML = "View from " + participantName + " participant";
-  InitSuperVizRoomWithComments(participantName);
-});
-
-document.getElementById("change-participant").addEventListener("click", changeParticipant);
-
-function changeParticipant() {
-  participantName = participantName == "Zeus" ? "Hera" : "Zeus";
-  document.getElementById("participant-name").innerHTML = "View from " + participantName + " participant";
-
-  room.destroy();
-  InitSuperVizRoomWithComments(participantName);
-}
-
-async function InitSuperVizRoomWithComments(participant) {
-  room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
-    roomId: roomId,
+async function initializeSuperVizRoom() {
+  const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
+    roomId: groupId,
     group: {
       id: groupId,
       name: groupName,
     },
     participant: {
-      id: participant.toLowerCase(),
-      name: participant,
+      id: participant.toString(),
+      name: "John " + participant,
     },
   });
 
-  const pinAdapter = new window.SuperVizRoom.CanvasPin(`participant-canvas`);
-  const comments = new window.SuperVizRoom.Comments(pinAdapter, {
-    position: "left",
-    buttonLocation: `top-left`,
-  });
-
+  const pinAdapter = new window.SuperVizRoom.HTMLPin("my-div");
+  const comments = new window.SuperVizRoom.Comments(pinAdapter);
   room.addComponent(comments);
+
+  return room;
 }
+
+initializeSuperVizRoom();
