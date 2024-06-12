9e25b953bdaf9a1460c18d8351d8c4ec084e3157
vítor norton <vtnorton@outlook.com>
2024-06-12 17:56:00 -0300

    Forms Elements (#9)
    * update scripts
    
    * testing script
    
    * update permission
    
    * update working-directory
    
    * Raised quality bar
    
    * improved performance
    
    * update script
    
    * update error code
    
    * Raised the bar
    
    * Update conditional
    
    * Fixed type error
    
    * Update beta
    
    * cdn autodesk
    
    * cdn autodesk comments
    
    * comments canvas
    
    * html comments
    
    * matterport comments
    
    * threejs comments
    
    * cdn matterport
    
    * cdn mouse
    
    * cdn real time
    
    * cdn three
    
    * video cdn
    
    * cdn wio
    
    * Update CDN Projects
    
    * update version
    
    * update type
    
    * Added Form Elements to samples
    
    * Fixed project infos
    
    * Fixed type check

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,47 +1,47 @@
-import { DEVELOPER_KEY, MATTERPORT_KEY } from "../env.js";
-import { sampleInfo } from "./projectInfo.js";
+import { DEVELOPER_KEY, MATTERPORT_KEY } from '../env.js'
+import { sampleInfo } from '../projectInfo.js'
 
-const participant = Math.floor(Math.random() * 100);
-const groupId = sampleInfo.id;
-const groupName = sampleInfo.name;
-const modelId = "LmRnZAsWoxy";
+const participant = Math.floor(Math.random() * 100)
+const groupId = sampleInfo.id
+const groupName = sampleInfo.name
+const modelId = 'LmRnZAsWoxy'
 
 function InitMatterport() {
-  const showcase = document.getElementById("participant");
-  if (!showcase) return;
+	const showcase = document.getElementById('participant')
+	if (!showcase) return
 
-  const showcaseWindow = showcase.contentWindow;
-  showcase.src = `./mp-bundle/showcase.html?&brand=0&mls=2&mt=0&search=0&kb=0&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=${modelId}`;
+	const showcaseWindow = showcase.contentWindow
+	showcase.src = `./mp-bundle/showcase.html?&brand=0&mls=2&mt=0&search=0&kb=0&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=${modelId}`
 
-  showcase.addEventListener("load", async () => {
-    if (!showcaseWindow) return;
-    const mpSDK = await showcaseWindow.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY);
+	showcase.addEventListener('load', async () => {
+		if (!showcaseWindow) return
+		const mpSDK = await showcaseWindow.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY)
 
-    InitSuperVizRoomWithMatterport(mpSDK, showcase);
-  });
+		InitSuperVizRoomWithMatterport(mpSDK, showcase)
+	})
 }
 
 async function InitSuperVizRoomWithMatterport(mpSDK, showcase) {
-  const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
-    roomId: groupId,
-    group: {
-      id: groupId,
-      name: groupName,
-    },
-    participant: {
-      id: participant.toString(),
-      name: "John " + participant,
-      avatar: {
-        imageUrl: `https://production.cdn.superviz.com/static/default-avatars/2.png`,
-        model3DUrl: `https://production.storage.superviz.com/readyplayerme/2.glb`,
-      },
-    },
-  });
+	const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
+		roomId: groupId,
+		group: {
+			id: groupId,
+			name: groupName,
+		},
+		participant: {
+			id: participant.toString(),
+			name: 'John ' + participant,
+			avatar: {
+				imageUrl: `https://production.cdn.superviz.com/static/default-avatars/2.png`,
+				model3DUrl: `https://production.storage.superviz.com/readyplayerme/2.glb`,
+			},
+		},
+	})
 
-  const pinAdapter = new window.MatterportPin(mpSDK, showcase);
-  const comments = new window.SuperVizRoom.Comments(pinAdapter);
+	const pinAdapter = new window.MatterportPin(mpSDK, showcase)
+	const comments = new window.SuperVizRoom.Comments(pinAdapter)
 
-  room.addComponent(comments);
+	room.addComponent(comments)
 }
 
-InitMatterport();
+InitMatterport()
