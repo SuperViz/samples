a13474f79150b6ce7e8f3186ce7ddd08d617ed02
vítor norton <vtnorton@outlook.com>
2024-06-12 17:56:00 -0300

    Forms Elements (#9)
    * update scripts
    
    * testing script
    
    * update permission
    
    * update working-directory
    
    * Raised quality bar
    
    * improved performance
    
    * update script
    
    * update error code
    
    * Raised the bar
    
    * Update conditional
    
    * Fixed type error
    
    * Update beta
    
    * cdn autodesk
    
    * cdn autodesk comments
    
    * comments canvas
    
    * html comments
    
    * matterport comments
    
    * threejs comments
    
    * cdn matterport
    
    * cdn mouse
    
    * cdn real time
    
    * cdn three
    
    * video cdn
    
    * cdn wio
    
    * Update CDN Projects
    
    * update version
    
    * update type
    
    * Added Form Elements to samples
    
    * Fixed project infos
    
    * Fixed type check

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,108 +1,108 @@
-import { DEVELOPER_KEY, CLIENT_ID, CLIENT_SECRET } from "../env.js";
-import { sampleInfo } from "./projectInfo.js";
+import { DEVELOPER_KEY, CLIENT_ID, CLIENT_SECRET } from '../env.js'
+import { sampleInfo } from '../projectInfo.js'
 
-const participant = Math.floor(Math.random() * 100);
-const groupId = sampleInfo.id;
-const groupName = sampleInfo.name;
+const participant = Math.floor(Math.random() * 100)
+const groupId = sampleInfo.id
+const groupName = sampleInfo.name
 
-const FORGE_CLIENT = CLIENT_ID;
-const FORGE_SECRET = CLIENT_SECRET;
+const FORGE_CLIENT = CLIENT_ID
+const FORGE_SECRET = CLIENT_SECRET
 
-const AUTH_URL = "https://developer.api.autodesk.com/authentication/v2/token";
-const modelURN = "urn:adsk.objects:os.object:e8d17563-1a4e-4471-bd72-a0a7e8d719bc/fileifc.ifc";
+const AUTH_URL = 'https://developer.api.autodesk.com/authentication/v2/token'
+const modelURN = 'urn:adsk.objects:os.object:e8d17563-1a4e-4471-bd72-a0a7e8d719bc/fileifc.ifc'
 const AutodeskData = {
-  grant_type: "client_credentials",
-  scope: "data:read bucket:read",
-};
-const token = btoa(`${FORGE_CLIENT}:${FORGE_SECRET}`);
+	grant_type: 'client_credentials',
+	scope: 'data:read bucket:read',
+}
+const token = btoa(`${FORGE_CLIENT}:${FORGE_SECRET}`)
 
 function InitAutodesk() {
-  const contentSection = document.getElementById("zeus-participant");
-  let viewer = null;
+	const contentSection = document.getElementById('zeus-participant')
+	let viewer = null
 
-  fetch(AUTH_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded", Authorization: `Basic ${token}` },
-    body: new URLSearchParams(AutodeskData).toString(),
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((dataToken) => {
-      const options = {
-        env: "AutodeskProduction2",
-        api: "streamingV2",
-        accessToken: dataToken.access_token,
-      };
+	fetch(AUTH_URL, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/x-www-form-urlencoded', Authorization: `Basic ${token}` },
+		body: new URLSearchParams(AutodeskData).toString(),
+	})
+		.then((res) => {
+			return res.json()
+		})
+		.then((dataToken) => {
+			const options = {
+				env: 'AutodeskProduction2',
+				api: 'streamingV2',
+				accessToken: dataToken.access_token,
+			}
 
-      const modelID = btoa(modelURN);
-      var documentId = `urn:${modelID}`;
+			const modelID = btoa(modelURN)
+			var documentId = `urn:${modelID}`
 
-      window.Autodesk.Viewing.Initializer(options, async () => {
-        viewer = new window.Autodesk.Viewing.GuiViewer3D(contentSection);
-        await viewer.start();
+			window.Autodesk.Viewing.Initializer(options, async () => {
+				viewer = new window.Autodesk.Viewing.GuiViewer3D(contentSection)
+				await viewer.start()
 
-        viewer.setTheme("dark-theme");
-        viewer.setQualityLevel(false, false);
-        viewer.setGhosting(false);
-        viewer.setGroundShadow(false);
-        viewer.setGroundReflection(false);
-        viewer.setOptimizeNavigation(true);
-        viewer.setProgressiveRendering(true);
+				viewer.setTheme('dark-theme')
+				viewer.setQualityLevel(false, false)
+				viewer.setGhosting(false)
+				viewer.setGroundShadow(false)
+				viewer.setGroundReflection(false)
+				viewer.setOptimizeNavigation(true)
+				viewer.setProgressiveRendering(true)
 
-        window.Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
-      });
-    });
+				window.Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure)
+			})
+		})
 
-  function onDocumentLoadSuccess(doc) {
-    const viewable = doc.getRoot().getDefaultGeometry();
-    if (viewable) {
-      viewer
-        .loadDocumentNode(doc, viewable, {
-          applyScaling: "meters",
-        })
-        .then(async (result) => {
-          await InitSuperVizRoomWithAutodesk(viewer);
-        })
-        .catch((error) => {
-          onDocumentLoadFailure(error);
-        });
-    }
-  }
-  function onDocumentLoadFailure(error) {
-    console.error(`Error loading forge model: ${error}`);
-  }
+	function onDocumentLoadSuccess(doc) {
+		const viewable = doc.getRoot().getDefaultGeometry()
+		if (viewable) {
+			viewer
+				.loadDocumentNode(doc, viewable, {
+					applyScaling: 'meters',
+				})
+				.then(async (result) => {
+					await InitSuperVizRoomWithAutodesk(viewer)
+				})
+				.catch((error) => {
+					onDocumentLoadFailure(error)
+				})
+		}
+	}
+	function onDocumentLoadFailure(error) {
+		console.error(`Error loading forge model: ${error}`)
+	}
 }
 
 async function InitSuperVizRoomWithAutodesk(viewer) {
-  const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
-    roomId: groupId,
-    group: {
-      id: groupId,
-      name: groupName,
-    },
-    participant: {
-      id: participant.toString(),
-      name: "John " + participant,
-      avatar: {
-        imageUrl: `https://production.cdn.superviz.com/static/default-avatars/2.png`,
-        model3DUrl: `https://production.storage.superviz.com/readyplayerme/2.glb`,
-      },
-    },
-  });
+	const room = await window.SuperVizRoom.init(DEVELOPER_KEY, {
+		roomId: groupId,
+		group: {
+			id: groupId,
+			name: groupName,
+		},
+		participant: {
+			id: participant.toString(),
+			name: 'John ' + participant,
+			avatar: {
+				imageUrl: `https://production.cdn.superviz.com/static/default-avatars/2.png`,
+				model3DUrl: `https://production.storage.superviz.com/readyplayerme/2.glb`,
+			},
+		},
+	})
 
-  const autodeskPresence = new window.Presence3D(viewer, {
-    isAvatarsEnabled: true,
-    isLaserEnabled: false,
-    isNameEnabled: true,
-    avatarConfig: {
-      height: 0,
-      scale: 10,
-      laserOrigin: { x: 0, y: 0, z: 0 },
-    },
-  });
+	const autodeskPresence = new window.Presence3D(viewer, {
+		isAvatarsEnabled: true,
+		isLaserEnabled: false,
+		isNameEnabled: true,
+		avatarConfig: {
+			height: 0,
+			scale: 10,
+			laserOrigin: { x: 0, y: 0, z: 0 },
+		},
+	})
 
-  room.addComponent(autodeskPresence);
+	room.addComponent(autodeskPresence)
 }
 
-InitAutodesk();
+InitAutodesk()
