facd2a8860882e3312be432e5189ead5b73ca2ec
Ian Silva <111044527+Raspincel@users.noreply.github.com>
2024-10-18 13:19:56 -0300

    feat: yjs samples (#11)
    * feat: yjs samples
    
    * feat: monaco vanilla samples
    
    * chore: update packages

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { SuperVizRoomProvider, useYjsProvider, YjsProvider } from '@superviz/react-sdk'
+import { sampleInfo } from './projectInfo'
+import * as Y from 'yjs'
+import { Editor } from '@monaco-editor/react'
+import { useEffect, useState } from 'react'
+import { MonacoBinding } from 'y-monaco'
+
+const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_KEY
+const groupId = sampleInfo.id
+const groupName = sampleInfo.name
+const participant = Math.floor(Math.random() * 100)
+	.toString()
+	.padStart(3, '0')
+
+const ydoc = new Y.Doc()
+
+const style = document.createElement('style')
+style.id = 'sv-yjs-monaco'
+document.head.appendChild(style)
+
+function Room() {
+	const { provider } = useYjsProvider()
+	const [editor, setEditor] = useState<any>(null)
+
+	useEffect(() => {
+		if (!provider || editor == null) return
+
+		const binding = new MonacoBinding(ydoc.getText('monaco'), editor.getModel()!, new Set([editor]), provider.awareness)
+
+		return () => {
+			binding.destroy()
+		}
+	}, [ydoc, provider, editor])
+
+	return (
+		<>
+			<YjsProvider doc={ydoc} />
+
+			<Editor
+				defaultValue='// Connect to the room to start collaborating'
+				defaultLanguage='typescript'
+				onMount={(editor) => {
+					setEditor(editor)
+				}}
+				options={{
+					padding: {
+						top: 32,
+					},
+				}}
+				theme='vs-dark'
+			/>
+		</>
+	)
+}
+
+function App() {
+	return (
+		<SuperVizRoomProvider
+			developerKey={DEVELOPER_KEY}
+			group={{
+				id: groupId,
+				name: groupName,
+			}}
+			participant={{
+				id: participant,
+				name: 'John ' + participant,
+			}}
+			roomId={groupId}
+			environment='dev'>
+			<Room />
+		</SuperVizRoomProvider>
+	)
+}
+
+export default App
